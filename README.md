61b0f4df8ccfe308c3c80fbc3bd3909062a8c495
Sascha Schwarze <schwarzs@de.ibm.com>
2023-11-03 11:08:37 +0100

    Enable image to call external URL before and after server shutdown to simulate finalization

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 An HTTP server with test methods for synthetic tests:
 
-* `GET /` is a hello-world
-* `GET /livecheck` returns 204 initially. `PUT /livecheck?code=newCode` changes this
-* `GET /sleep?delay=5` will return a 204 after five seconds
-* `/ws` endpoint serves a websocket with an echo service that return everything that is send to it
+* `GET /` is a hello-world.
+* `GET /call-after-server-shutdown?url=https://someurl` returns a 204 and will cause the provided URL to be called after stopping the server. Optionally, a `delay` can be provided to override a wait time in seconds (default is 5s).
+* `GET /before-server-shutdown?url=https://someurl` returns a 204 and will cause the provided URL to be called when a SIGTERM signal is captured and before stopping the server. Optionally, a `delay` can be provided to override a wait time in seconds (default is 5s).
+* `GET /livecheck` returns 204 initially. `PUT /livecheck?code=newCode` changes this.
+`delay` argument can be provided which overrides the default five seconds that it waits before it performs the call.
+* `GET /sleep?delay=5` will return a 204 after five seconds.
+* `/ws` endpoint serves a websocket with an echo service that return everything that is send to it.
--- main.go
+++ main.go
@@ -32,11 +32,61 @@ func main() {
 
 	srv := &http.Server{Addr: fmt.Sprintf(":%d", port)}
 
+	var afterShutDownUrl *string
+	afterShutDownUrlDelay := 5 * time.Second
+
 	livecheckCode := 204
+
+	var sigtermUrl *string
+	sigtermUrlDelay := 5 * time.Second
 
 	go func() {
 		http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 			fmt.Fprintf(w, "Hello, World! I am using %s by the way.", runtime.Version())
+		})
+
+		http.HandleFunc("/call-after-server-shutdown", func(w http.ResponseWriter, request *http.Request) {
+			queryParameters := request.URL.Query()
+			if !queryParameters.Has("url") {
+				w.WriteHeader(400)
+				return
+			}
+
+			url := queryParameters.Get("url")
+			afterShutDownUrl = &url
+
+			if queryParameters.Has("delay") {
+				delay, err := strconv.Atoi(queryParameters.Get("delay"))
+				if err != nil {
+					w.WriteHeader(400)
+					return
+				}
+				afterShutDownUrlDelay = time.Duration(delay) * time.Second
+			}
+
+			w.WriteHeader(204)
+		})
+
+		http.HandleFunc("/call-before-server-shutdown", func(w http.ResponseWriter, request *http.Request) {
+			queryParameters := request.URL.Query()
+			if !queryParameters.Has("url") {
+				w.WriteHeader(400)
+				return
+			}
+
+			url := queryParameters.Get("url")
+			sigtermUrl = &url
+
+			if queryParameters.Has("delay") {
+				delay, err := strconv.Atoi(queryParameters.Get("delay"))
+				if err != nil {
+					w.WriteHeader(400)
+					return
+				}
+				sigtermUrlDelay = time.Duration(delay) * time.Second
+			}
+
+			w.WriteHeader(204)
 		})
 
 		http.HandleFunc("/livecheck", func(w http.ResponseWriter, request *http.Request) {
@@ -93,8 +143,49 @@ func main() {
 	}()
 
 	<-signals
+
+	log.Print("Received SIGTERM")
+
+	if sigtermUrl != nil {
+		time.Sleep(sigtermUrlDelay)
+
+		log.Printf("Calling before server close: %s", *sigtermUrl)
+		resp, err := http.Get(*sigtermUrl)
+
+		if err != nil {
+			log.Printf("Failed to call %s before server close: %v", *sigtermUrl, err)
+		}
+
+		if resp != nil {
+			if resp.StatusCode > 299 {
+				log.Printf("Failed to call %s before server close with status code %d", *sigtermUrl, resp.StatusCode)
+			} else {
+				log.Printf("Successfully called %s before server close with status %d", *sigtermUrl, resp.StatusCode)
+			}
+		}
+	}
+
 	log.Printf("shutting down server")
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatalf("failed to shutdown server: %v", err)
+	}
+
+	if afterShutDownUrl != nil {
+		time.Sleep(afterShutDownUrlDelay)
+
+		log.Printf("Calling after server close: %s", *afterShutDownUrl)
+		resp, err := http.Get(*afterShutDownUrl)
+
+		if err != nil {
+			log.Printf("Failed to call %s after server close: %v", *afterShutDownUrl, err)
+		}
+
+		if resp != nil {
+			if resp.StatusCode > 299 {
+				log.Printf("Failed to call %s after server close with status code %d", *afterShutDownUrl, resp.StatusCode)
+			} else {
+				log.Printf("Successfully called %s after server close with status %d", *afterShutDownUrl, resp.StatusCode)
+			}
+		}
 	}
 }
